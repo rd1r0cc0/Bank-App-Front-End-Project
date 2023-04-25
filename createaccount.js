b87594914c78f1d1a370db1d4135c7952f41eb60
rd1r0cc0 <39325791+rd1r0cc0@users.noreply.github.com>
2023-04-25 08:08:57 -0500

    Initial Upload

---
+++ createaccount.js
@@ -0,0 +1,60 @@
+function CreateAccount(){
+  const [show, setShow]         = React.useState(true);
+  const [status, setStatus]     = React.useState('');
+  const [name, setName]         = React.useState('');
+  const [email, setEmail]       = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const ctx = React.useContext(UserContext);  
+
+  function validate(field, label){
+      if (!field) {
+        setStatus('Error: ' + label);
+        setTimeout(() => setStatus(''),3000);
+        return false;
+      }
+      return true;
+  }
+
+  function handleCreate(){
+    console.log(name,email,password);
+    if (!validate(name,     'name'))     return;
+    if (!validate(email,    'email'))    return;
+    if (!validate(password, 'password')) return;
+    ctx.users.push({name,email,password,balance:100});
+    setShow(false);
+  }    
+
+  function clearForm(){
+    setName('');
+    setEmail('');
+    setPassword('');
+    setShow(true);
+  }
+
+  return (
+    <Card
+      bgcolor="primary"
+      header="Create Account"
+      status={status}
+      body={show ? (  
+              <form>
+              Name<br/>
+              <input type="text" className="form-control" id="name" placeholder="Enter name"
+              value={name} onChange={e => setName(e.currentTarget.value)} autoComplete="name"/><br/>
+              Email address<br/>
+              <input type="input" className="form-control" id="email" placeholder="Enter email"
+              value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+              Password<br/>
+              <input type="password" className="form-control" id="password" placeholder="Enter password"
+              value={password} onChange={e => setPassword(e.currentTarget.value)} autoComplete="new-password"/><br/>
+              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+              </form>
+            ):(
+              <>
+              <h5>Success</h5>
+              <button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
+              </>
+            )}
+    />
+  )
+}
